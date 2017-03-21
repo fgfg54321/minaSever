22caf06f6b1c7b6df61a19e2b2c722ce425a4dbd
luoyecao <371642748@qq.xom>
2017-03-21 23:36:21 +0800

    完善文档

--- src/org/apache/mina/tcp/base/struct/ConnectClient.java
+++ src/org/apache/mina/tcp/base/struct/ConnectClient.java
@@ -4,6 +4,10 @@ import org.apache.mina.tcp.base.transserver.TServerConfig;
 
 public class ConnectClient extends ConnectBase
 {
+
+	public Route             route;
+	public Route             fromRoute;
+	
 	public ConnectClient()
 	{
 		type = TServerConfig.TYPE_CLIENT;
