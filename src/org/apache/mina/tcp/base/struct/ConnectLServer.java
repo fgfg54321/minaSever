5fd95e3172d5b6a7fd3f90485b7b9efac601f659
luoyecao <371642748@qq.xom>
2017-03-22 23:38:33 +0800

    修改bug

--- src/org/apache/mina/tcp/base/struct/ConnectLServer.java
+++ src/org/apache/mina/tcp/base/struct/ConnectLServer.java
@@ -4,8 +4,6 @@ import org.apache.mina.tcp.base.transserver.TServerConfig;
 
 public class ConnectLServer extends ConnectBase
 {
-	public Route             route;
-	public Route             fromRoute;
 	
 	public ConnectLServer()
 	{
