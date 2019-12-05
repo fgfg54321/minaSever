e4da13150632e4128d593835a74dcbdd5935a50b
luoyecao <371642748@qq.com>
2019-12-05 14:56:27 +0800

    ss

--- src/org/apache/mina/tcp/base/utils/WebConfigLoader.java
+++ src/org/apache/mina/tcp/base/utils/WebConfigLoader.java
@@ -1,18 +1,71 @@
 package org.apache.mina.tcp.base.utils;
 
-import java.nio.charset.Charset;
-
-import org.apache.mina.log.MLog;
-import org.apache.mina.utils.Action;
-import org.asynchttpclient.AsyncCompletionHandler;
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.BoundRequestBuilder;
-import org.asynchttpclient.DefaultAsyncHttpClient;
-import org.asynchttpclient.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
 
 public class WebConfigLoader
 {
+	public static void main(String[] args) throws IOException 
+    {
+		//LoadFormRemote(String url,Action<Response> onCompleted)
+		
+		String result = sendGet("http://redlist2.warfarestrike.com/info/HttpInfo","logic=HttpAnnouncement&appId=1001&channelId=8888888&serviceId=12292&uid=2999612&language=ara");
+		System.out.println(result);
+    }
+	
 
+	public static String sendGet(String url, String param) {
+        String result = "";
+        BufferedReader in = null;
+        try {
+            String urlNameString = url + "?" + param;
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent",
+                    "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+            for (String key : map.keySet()) {
+                System.out.println(key + "--->" + map.get(key));
+            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream()));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+	
+	
+	/*
 	public void LoadFromLocal(String file)
 	{
 		
@@ -20,18 +73,14 @@ public class WebConfigLoader
 	
 	public void LoadFormRemote(String url,Action<Response> onCompleted)
 	{
-		
 		AsyncHttpClient asyncHttpClient    = new DefaultAsyncHttpClient();
-		BoundRequestBuilder requestBuilder = asyncHttpClient.preparePost(url);
-		requestBuilder.addFormParam(name, value);
+		BoundRequestBuilder requestBuilder = asyncHttpClient.prepareGet(url);
 		requestBuilder.execute(new AsyncCompletionHandler<Response>(){
 		    
 		    @Override
 		    public Response onCompleted(Response response) throws Exception
 		    {
-		        
 		    	String text = response.getResponseBody(Charset.forName("UTF-8"));
-		    	
 		        return response;
 		    }
 		    
@@ -43,4 +92,5 @@ public class WebConfigLoader
 		    }
 		});
 	}
+	*/
 }
